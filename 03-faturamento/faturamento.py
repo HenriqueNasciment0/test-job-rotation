64fc803a2af25bf334f74b543beae43ae76303f2
HenriqueNasciment0 <you@example.com>
2023-03-08 21:13:41 -0300

    exercicio 3, printa na tela o maior e menor faturamento e mostra quantos dias a empresa teve acima da média

--- 03-faturamento/faturamento.py
+++ 03-faturamento/faturamento.py
@@ -5,6 +5,8 @@ with open("faturamento.json", encoding='utf-8') as f:
 
 total = 0
 arrayFaturamento = list()
+baseMedia = 0
+qntDias = 0
 
 for i in data:
     total = total + i['faturamento']
@@ -13,9 +15,19 @@ for i in data:
 for i in arrayFaturamento:
     if i == 0:
         arrayFaturamento.remove(i)
+    baseMedia = baseMedia + i
+
+mediaMensal = round(baseMedia / len(arrayFaturamento))
+
+for i in arrayFaturamento:
+
+    if i > mediaMensal:
+        qntDias = qntDias + 1
 
 minFaturamento = min(arrayFaturamento)
 maxFaturamento = max(arrayFaturamento)
 
-print(minFaturamento)
-print(maxFaturamento)
+
+print(f'O menor faturamento no mês foi de: ${minFaturamento}.\n')
+print(f'O maior faturamento no mês foi de: ${maxFaturamento}.\n')
+print(f'Tivemos nesse mês, {qntDias} dias com faturamento acima da média mensal!\n')
